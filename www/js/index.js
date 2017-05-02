7540f70176581f3d6f33d956e7b88f6e67a16d8f
Luca de Alfaro <luca@dealfaro.org>
2017-05-01 23:46:30 -0700

    initial commit

---
+++ www/js/index.js
@@ -0,0 +1,72 @@
+
+var app = function() {
+
+    var self = {};
+    self.is_configured = false;
+
+    Vue.config.silent = false; // show all warnings
+
+    // Extends an array
+    self.extend = function(a, b) {
+        for (var i = 0; i < b.length; i++) {
+            a.push(b[i]);
+        }
+    };
+
+    // Enumerates an array.
+    var enumerate = function(v) {
+        var k=0;
+        v.map(function(e) {e._idx = k++;});
+    };
+
+    // Initializes an attribute of an array of objects.
+    var set_array_attribute = function (v, attr, x) {
+        v.map(function (e) {e[attr] = x;});
+    };
+
+    self.initialize = function () {
+        document.addEventListener('deviceready', self.ondeviceready, false);
+    };
+
+    self.ondeviceready = function () {
+        // This callback is called once Cordova has finished its own initialization.
+        console.log("The device is ready");
+        $("#vue-div").show();
+        // Gets the file for storing the secret.
+        self.identityFile = null;
+        self.user_identity = null;
+        var platform = device.platform;
+        console.log('platform: ' + platform);
+
+        // Initializes identity.
+        if (platform == "browser") {
+            self.user_identity = "test-browser";
+        } else {
+            self.user_identity = device.uuid;
+        }
+        console.log("Identity: " + self.user_identity);
+        self.is_configured = true;
+    };
+
+    self.vue = new Vue({
+        el: "#vue-div",
+        delimiters: ['${', '}'],
+        unsafeDelimiters: ['!{', '}'],
+        data: {
+        },
+        methods: {
+        }
+
+    });
+
+    return self;
+};
+
+var APP = null;
+
+// This will make everything accessible from the js console;
+// for instance, self.x above would be accessible as APP.x
+jQuery(function(){
+    APP = app();
+    APP.initialize();
+});
